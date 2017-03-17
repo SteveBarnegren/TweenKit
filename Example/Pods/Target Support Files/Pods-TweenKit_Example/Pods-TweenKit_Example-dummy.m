1157f6eafdb8d27e4f36fb2e09c1763a8af482e7
Steve Barnegren <steve.barnegren@gmail.com>
2017-03-17 20:52:45 +0000

    Project setup

---
+++ Example/Pods/Target Support Files/Pods-TweenKit_Example/Pods-TweenKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TweenKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_TweenKit_Example
+@end
---
+++ Example/Pods/Target Support Files/TweenKit/TweenKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TweenKit : NSObject
+@end
+@implementation PodsDummy_TweenKit
+@end
