90b78d2144e8a77f623c7edddf8fac924a237328
Steve Barnegren <steve.barnegren@gmail.com>
2017-03-17 21:02:47 +0000

    Changed project structure

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Steve Barnegren on 17/03/2017.
+//  Copyright © 2017 Steve Barnegren. All rights reserved.
+//
+
+import UIKit
+import TweenKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+    }
+
+   
+}
+
