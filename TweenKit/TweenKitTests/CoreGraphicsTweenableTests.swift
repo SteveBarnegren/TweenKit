7d690ade93ceff4d9c9fe38da927f5e3814ec5a3
Steve Barnegren <steve.barnegren@gmail.com>
2017-04-06 10:16:23 +0100

    Added some tests for Tweenable implementations

---
+++ TweenKit/TweenKitTests/CoreGraphicsTweenableTests.swift
@@ -0,0 +1,82 @@
+//
+//  CoreGraphicsTweenableTests.swift
+//  TweenKit
+//
+//  Created by Steven Barnegren on 06/04/2017.
+//  Copyright © 2017 Steve Barnegren. All rights reserved.
+//
+
+import XCTest
+@testable import TweenKit
+
+class CoreGraphicsTweenableTests: XCTestCase {
+    
+    // MARK: - CGFloat
+    
+    func testCGFloatLerpResultsInCorrectValue() {
+    
+        let start = CGFloat(2)
+        let end = CGFloat(4)
+        XCTAssertEqualWithAccuracy(start.lerp(t: 0.5, end: end), CGFloat(3), accuracy: 0.001)
+    }
+    
+    // MARK: - CGPoint
+    
+    func testCGPointLerpResultsInCorrectValue() {
+        
+        let start = CGPoint(x: 2, y: 2)
+        let end = CGPoint(x: 4, y: 4)
+        AssertCGPointsAreEqualWithAccuracy(start.lerp(t: 0.5, end: end),
+                                           CGPoint(x: 3, y: 3),
+                                           accuracy: 0.001)
+    }
+    
+    func testCGPointCanBeConstructedAsTweenable2DCoordinate() {
+    
+        let x = CGFloat(2)
+        let y = CGFloat(3)
+        
+        let normal = CGPoint(x: x, y: y)
+        let tweenable = CGPoint(tweenableX: Double(x), y: Double(y))
+
+        AssertCGPointsAreEqualWithAccuracy(normal, tweenable, accuracy: 0.001)
+    }
+    
+    func testCGPointReturnsCorrectXAndYAsTweenableCoordinate() {
+        
+        let x = CGFloat(2)
+        let y = CGFloat(5)
+        
+        let point = CGPoint(x: x, y: y)
+       
+        XCTAssertEqualWithAccuracy(point.tweenableX, Double(x), accuracy: 0.001)
+        XCTAssertEqualWithAccuracy(point.tweenableY, Double(y), accuracy: 0.001)
+    }
+    
+    // MARK: - CGSize
+    
+    func testCGSizeLerpResultsInCorrectValue() {
+        
+        let start = CGSize(width: 2, height: 10)
+        let end = CGSize(width: 4, height: 20)
+        
+        let result = start.lerp(t: 0.5, end: end)
+        let expected = CGSize(width: 3, height: 15)
+        
+        AssertCGSizesAreEqualWithAccuracy(result, expected, accuracy: 0.001)
+    }
+    
+    // MARK: - CGRect
+    
+    func testCGRectLerpResultsInCorrectValue() {
+        
+        let start = CGRect(x: 10, y: 20, width: 30, height: 40)
+        let end = CGRect(x: 20, y: 30, width: 40, height: 50)
+
+        let result = start.lerp(t: 0.5, end: end)
+        let expected = CGRect(x: 15, y: 25, width: 35, height: 45)
+        
+        AssertCGRectsAreEqualWithAccuracy(result, expected, accuracy: 0.001)
+    }
+    
+}
--- TweenKit/TweenKitTests/XCTestCase+CustomAssertions.swift
+++ TweenKit/TweenKitTests/XCTestCase+CustomAssertions.swift
@@ -88,6 +88,52 @@ extension XCTestCase {
         
         XCTFail(message, file: file, line: line)
     }
-
+    
+    func AssertCGSizesAreEqualWithAccuracy(_ s1: CGSize,
+                                           _ s2: CGSize,
+                                           accuracy: Double,
+                                           file: StaticString = #file,
+                                           line: UInt = #line) {
+        
+        let wDiff = abs(s2.width - s1.width)
+        let hDiff = abs(s2.height - s1.height)
+        
+        let allowedDifference = 0.001
+        
+        if Double(wDiff) < allowedDifference && Double(hDiff) < allowedDifference {
+            return
+        }
+        
+        let message = "(\(s1.width), \(s1.height)) is not equal to (\(s2.width, s2.height)))"
+        
+        XCTFail(message, file: file, line: line)
+    }
+    
+    func AssertCGRectsAreEqualWithAccuracy(_ r1: CGRect,
+                                           _ r2: CGRect,
+                                           accuracy: Double,
+                                           file: StaticString = #file,
+                                           line: UInt = #line) {
+        
+        let xDiff = abs(r2.origin.x - r1.origin.x)
+        let yDiff = abs(r2.origin.y - r1.origin.y)
+        let wDiff = abs(r2.size.width - r1.size.width)
+        let hDiff = abs(r2.size.height - r1.size.height)
+        
+        let allowedDifference = 0.001
+        
+        if Double(xDiff) < allowedDifference
+            && Double(yDiff) < allowedDifference
+            && Double(wDiff) < allowedDifference
+            && Double(hDiff) < allowedDifference{
+            return
+        }
+        
+        let message = "\(r1) is not equal to \(r2)"
+        
+        XCTFail(message, file: file, line: line)
+    }
+    
+    
 }
 
