7477a8c98423f538099d917d7266dd9c6cdcc5e5
Steve Barnegren <steve.barnegren@gmail.com>
2020-09-04 20:18:52 +0100

    Added macOS example project

---
+++ Example-macOS/Example-macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Example-macOS
+//
+//  Created by Steve Barnegren on 04/09/2020.
+//  Copyright © 2020 Steve Barnegren. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Example-macOS/Example-macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Example-macOS
+//
+//  Created by Steve Barnegren on 04/09/2020.
+//  Copyright © 2020 Steve Barnegren. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
